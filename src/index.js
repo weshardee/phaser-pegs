7bf8697c79844315715325b63b89a97dbc38e2d2
Wes Hardee <weshardee@gmail.com>
2015-11-15 11:02:15 -0600

    remove game class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import GameState from './states/GameState';
 
-class Game extends Phaser.Game {
-    constructor() {
-        super(500, 500, Phaser.AUTO, 'content', null);
-        this.state.add('GameState', GameState, false);
-        this.state.start('GameState');
-    }
+const game = new Phaser.Game(500, 500, Phaser.AUTO, 'peg-game', {
+    create,
+    update,
+});
+game.state.add('GameState', GameState, false);
+game.state.start('GameState');
+
+export default game;
+
+function create() {
+
 }
 
-new Game();
+function update() {
+
+}
